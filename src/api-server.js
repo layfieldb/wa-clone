304090947d8a347d782e5437beed0b6db6e5aadf
layfieldb <ben.layfield@gmail.com>
2016-11-09 12:16:41 -0500

    adding redis-server.js

--- src/api-server.js
+++ src/api-server.js
@@ -1,12 +1,30 @@
-const express = require('express');
+import express from 'express';
+import { setupRedis, redisClient } from './redis-client';
+
+if (!process.env.PORT) {
+  require('dotenv').load();
+  console.log('.env loaded manually, port', process.env.PORT);
+}
 
 const app = express();
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
+
+app.get('redis-test', (req, res) => {
+  redisClient.incr('inc-test', (err, result) => {
+    if (err) {
+      res.send('Error incrementing redis test');
+    } else {
+      res.send(`Redis increment test result: ${result}`);
+    }
+  });
+});
+
+setupRedis();
 
 app.listen(port, () => {
     console.log(`Server running at port ${port}.`);
